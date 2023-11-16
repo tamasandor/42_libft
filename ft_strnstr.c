700892185385c0cc964a5603da64cdfffa37a96f
atamas <atamas@student.42wolfsburg.de>
2023-11-16 16:22:47 +0100

    add ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   !!ft_strnstr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 14:05:37 by atamas            #+#    #+#             */
+/*   Updated: 2023/11/16 16:21:51 by atamas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *big_string, const char *small_string, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (*small_string == '\0')
+		return ((char *)big_string);
+	while (i < len && big_string[i] != '\0')
+	{
+		if (big_string[i] == small_string[j])
+		{
+			j++;
+		}
+		else
+			j = 0;
+		i++;
+	}
+	i -= j;
+	if (j != 0)
+		return ((char *)big_string + i);
+	return (0);
+}
