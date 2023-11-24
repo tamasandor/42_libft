99c791bd81369fb110f49ac8f8e8bb41406decb0
atamas <atamas@student.42wolfsburg.de>
2023-11-24 14:49:09 +0100

    add test case and function description to ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,24 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:35:26 by atamas            #+#    #+#             */
-/*   Updated: 2023/11/14 18:53:30 by atamas           ###   ########.fr       */
+/*   Updated: 2023/11/24 14:48:20 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *b, int c, int len)
+#include <stddef.h>
+/* #include <stdio.h>
+#include <string.h> */
+
+/* 
+Writes len bytes of value c (converted to an unsigned char) to the
+	string b
+Return: First argument
+ */
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*ptr;
 
-	ptr = (unsigned char *)b;
+	ptr = (unsigned char *) b;
 	while (len != 0)
 	{
 		*ptr = (unsigned char) c;
@@ -23,3 +32,20 @@ void	*ft_memset(void *b, int c, int len)
 	}
 	return (b);
 }
+
+/* int main() {
+	char str[20];
+	char expected[20];
+	int value;
+	size_t len;
+
+	value = 'A';
+	len = 10;
+	memset(expected, value, len);
+	ft_memset(str, value, len);
+	if (memcmp(str, expected, len) == 0) {
+		printf("ft_memset is correct!\n");
+	} else {
+		printf("ft_memset is incorrect!\n");
+	}
+} */
