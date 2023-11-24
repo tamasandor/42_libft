0488fa14588ba9188a24f8bc769afb3cdd267fbd
atamas <atamas@student.42wolfsburg.de>
2023-11-24 12:22:21 +0100

    Add test case and func description for ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,16 +6,19 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 21:01:52 by atamas            #+#    #+#             */
-/*   Updated: 2023/11/13 21:25:56 by atamas           ###   ########.fr       */
+/*   Updated: 2023/11/24 12:21:52 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// void *ft_memchr(const void *s, int c, int n)
+#include <stddef.h>
+/* #include <stdio.h> */
 
-// >> locates the firsst occurence of c converted to unsigned char in string s
-// return the pointer to the loaction or NULL if none;q
-
-void	*ft_memchr(const void *s, int c, int n)
+/* 
+Locates the first occurence of the character c within n bytes
+Input: pointer to the array
+Return: pointer to the byte located or NULL if no such byte exists within n bytes
+ */
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
 
@@ -29,3 +32,26 @@ void	*ft_memchr(const void *s, int c, int n)
 	}
 	return (0);
 }
+
+/* int	main(void)
+{
+	const char	*string;
+	int			search_char;
+	int			length;
+	char		*result;
+
+	string = "Chasing dreams like a fox chasing fireflies in the twilight";
+	search_char = 'x';
+	length = 25;
+	result = ft_memchr(string, search_char, length);
+	if (result != NULL)
+	{
+		printf("Character '%c' found. \n", search_char);
+		printf("Position is: %ld\n", (char *)result - string);
+	}
+	else
+	{
+		printf("'%c' was not in the first %d char.\n", search_char, length);
+	}
+}
+ */
