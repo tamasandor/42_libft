9bf371a377d5167b29799291096ed0c941d33387
atamas <atamas@student.42wolfsburg.de>
2023-11-29 14:55:23 +0100

    Working without segmentation fault on purpose for ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:05:37 by atamas            #+#    #+#             */
-/*   Updated: 2023/11/28 20:40:00 by atamas           ###   ########.fr       */
+/*   Updated: 2023/11/29 14:54:56 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 char	*ft_strnstr(const char *big_str, const char *small_str, size_t len)
 {
 	size_t	i;
-	size_t	j;
-	size_t	len_of_small_str;
+	size_t	small_len;
 
 	i = 0;
 	if (*small_str == '\0')
 		return ((char *)big_str);
-	len_of_small_str = ft_strlen(small_str);
+	small_len = ft_strlen(small_str);
 	if (len < 0)
 		len = ft_strlen(big_str);
 	while (i < len)
 	{
-		
+		if (i + small_len > len)
+			return (0);
+		if (big_str[i] == *small_str)
+		{
+			if (ft_strncmp((char *)big_str + i, (char *)small_str, small_len) == 0)
+				return ((char *)&big_str[i]);
+		}
 		i++;
 	}
 	return (0);
@@ -43,5 +48,4 @@ int	main(void)
 	// retstr = ft_strnstr(bigstr, "a", 12); test case fails it should not
 	retstr = ft_strnstr(bigstr, "alma", 12);
 	printf("String is: %s", retstr);
-}
- */ 
+} */
