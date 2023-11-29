dc5ed13245f639100a6c4280f14fae134138466b
atamas <atamas@student.42wolfsburg.de>
2023-11-29 15:15:17 +0100

    segmentation fault on purpose for ft_strnstr because of testers

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 14:05:37 by atamas            #+#    #+#             */
-/*   Updated: 2023/11/29 14:54:56 by atamas           ###   ########.fr       */
+/*   Updated: 2023/11/29 15:05:51 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,22 @@
 
 char	*ft_strnstr(const char *big_str, const char *small_str, size_t len)
 {
-	size_t	i;
 	size_t	small_len;
 
-	i = 0;
 	if (*small_str == '\0')
 		return ((char *)big_str);
 	small_len = ft_strlen(small_str);
 	if (len < 0)
 		len = ft_strlen(big_str);
-	while (i < len)
+	while (*big_str && small_len <= len)
 	{
-		if (i + small_len > len)
-			return (0);
-		if (big_str[i] == *small_str)
+		if (*big_str == *small_str)
 		{
-			if (ft_strncmp((char *)big_str + i, (char *)small_str, small_len) == 0)
-				return ((char *)&big_str[i]);
+			if (!ft_strncmp((char *)big_str, (char *)small_str, small_len))
+				return ((char *)big_str);
 		}
-		i++;
+		len--;
+		big_str++;
 	}
 	return (0);
 }
