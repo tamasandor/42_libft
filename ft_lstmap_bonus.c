0db323e6f6a31e7aab108a8b631fb5d8a7c409ff
atamas <atamas@student.42wolfsburg.de>
2023-12-01 20:40:31 +0100

    Complete libft

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: atamas <atamas@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 19:57:39 by atamas            #+#    #+#             */
-/*   Updated: 2023/12/01 20:18:28 by atamas           ###   ########.fr       */
+/*   Updated: 2023/12/01 20:35:27 by atamas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,22 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*newone;
+	t_list	*temp;
+	void	*cont;
 
-	ft_lstiter(lst, f);
-	newone = ft_lstnew(lst->content);
+	newone = NULL;
 	while (lst)
 	{
-		if (!newone)
+		cont = f(lst->content);
+		temp = ft_lstnew(cont);
+		if (!temp)
 		{
+			del(cont);
 			ft_lstclear(&newone, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&newone, lst);
+		ft_lstadd_back(&newone, temp);
+		lst = lst->next;
 	}
 	lst = NULL;
 	return (newone);
